481ff52bc11ff65be73f292cf3176785706c9dbb
mrvishalsharma <mrvishalsharma8@gmail.com>
2021-10-15 08:27:37 +0530

    added solution of insert problem

--- Array/insert.cpp
+++ Array/insert.cpp
@@ -8,5 +8,17 @@
 
 // Note:- We cannot insert in fixed size array we can onlly insert in dynamic size array
 
+// Solution:-
 
+int insert(int arr[], int n, int x, int cap,int pos){
+	if(n==cap) return n;
+
+	int idx = pos-1; //index where we want to insert the element
+	for(int i=n-1; i>=idx; i--){
+		arr[i+1]=arr[i];
+		arr[idx]=x;
+
+	}
+	return (n+1);
+}
      
