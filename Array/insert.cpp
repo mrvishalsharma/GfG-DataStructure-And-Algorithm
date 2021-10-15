3fad407c0a67517691991f22b2bef03cb60ca213
mrvishalsharma <mrvishalsharma8@gmail.com>
2021-10-15 08:23:41 +0530

    added insert problem of gfg

---
+++ Array/insert.cpp
@@ -0,0 +1,12 @@
+// Write a program to insert a number in an array
+
+// Ex:- Ip arr[]={5,10,20,_,_};
+//      x   = 7
+//      pos = 2
+
+// Output:- arr[]={5,7,10,20,_}
+
+// Note:- We cannot insert in fixed size array we can onlly insert in dynamic size array
+
+
+     
